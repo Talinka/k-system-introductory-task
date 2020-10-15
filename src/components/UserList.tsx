ee0654beafa82f3a321e37c4432e5688ebbf23dc
talita <polikashina@gmail.com>
2020-10-16 01:09:36 +0200

    push click handler throw user list

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -7,12 +7,13 @@ import User from '../types/User';
 
 type UserListProps = {
   users: Array<User>,
+  onClick: (user: User) => void;
 }
 
-const UserList = ({ users }: UserListProps) => {
+const UserList = ({ users, onClick }: UserListProps) => {
   return (
     <List>
-      {users.map((user) => <UserItem user={user} key={user.id} />)}
+      {users.map((user) => <UserItem user={user} key={user.id} onClick={onClick}/>)}
     </List>
   );
 }

e63085a677742ad12b80c86c3cccdc6883f97764
talita <polikashina@gmail.com>
2020-10-16 01:22:41 +0200

    add main component

---
+++ src/components/Main.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+import UserList from './UserList';
+import UserStatus from '../types/UserStatus';
+import ChangeUserStatusModal from './modals/ChangeUserStatus';
+
+import User from '../types/User';
+
+type MainProps = {
+  users: Array<User>,
+}
+
+const Main = ({ users }: MainProps) => {
+  const [open, setOpen] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(users[0]);
+
+  const changeStatusSubmit = (newStatus: UserStatus, isSaveChange: boolean) => {
+    if (isSaveChange) {
+      console.log(newStatus);
+    }
+    setOpen(false);
+  }
+
+  const handleShowDialog = (user: User) => {
+    setSelectedUser(user);
+    setOpen(true);
+  }
+
+  return (
+    <div>
+      <h2>User list</h2>
+      <UserList users={users} onClick={handleShowDialog} />
+      <ChangeUserStatusModal user={selectedUser} open={open} currentStatus={selectedUser.status} onSubmit={changeStatusSubmit}
+      />
+    </div>
+  );
+}
+
+export default Main;
--- src/components/modals/ChangeUserStatus.tsx
+++ src/components/modals/ChangeUserStatus.tsx
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
 import Button from '@material-ui/core/Button'
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
+// components
+import StatusGroup from '../StatusGroup';
 // types
 import User from '../../types/User';
 import UserStatus from '../../types/UserStatus';
@@ -16,55 +14,36 @@ import UserStatus from '../../types/UserStatus';
 type ChangeUserStatusProps = {
   user: User,
   open: boolean,
-  onSubmit: (newStatus: UserStatus | null) => void,
+  currentStatus: UserStatus,
+  onSubmit: (newStatus: UserStatus, isChanged: boolean) => void,
 };
 
-const statuses: Array<UserStatus> = [
-  UserStatus.New,
-  UserStatus.Verified,
-  UserStatus.VIP,
-];
+const ChangeUserStatus = ({ user, currentStatus, open, onSubmit }: ChangeUserStatusProps) => {
+  let { name } = user;
+  const [selectedStatus, setStatus] = useState(currentStatus);
 
-const ChangeUserStatus = ({ user, open, onSubmit }: ChangeUserStatusProps) => {
-  const { name, status: oldStatus } = user;
-  const [statusText, setStatusText] = useState(oldStatus.toString());
-  const handleSave = () => {
-    const newStatus = statuses.find((status) => status.toString() === statusText);
-    onSubmit(newStatus ?? null);
+  const handleSubmit = (isSaveChange: boolean) => () => {
+    onSubmit(selectedStatus, isSaveChange);
   };
-  const handleCancel = () => onSubmit(null);
 
-  const handleChange = (e: React.ChangeEvent<{}>, value: string) => {
-    setStatusText(value);
+  const handleStatusChange = (status: UserStatus) => {
+    setStatus(status);
   };
 
   return (
-    <Dialog open={open} onClose={handleCancel} aria-labelledby="form-dialog-title">
+    <Dialog open={open} onClose={handleSubmit(false)} aria-labelledby="form-dialog-title">
       <DialogTitle id="form-dialog-title">Change user status</DialogTitle>
       <DialogContent>
         <DialogContentText>
           {`Select status for user ${name}:`}
         </DialogContentText>
-        <FormControl>
-          <RadioGroup row aria-label="status" name="status" value={statusText} onChange={handleChange}>
-            {statuses
-              .map((status, i) => (
-                <FormControlLabel
-                  key={i}
-                  value={status.toString()}
-                  control={<Radio />}
-                  label={status.toString()}
-                />
-              ))
-            }
-          </RadioGroup>
-        </FormControl>
+        <StatusGroup status={user.status} onStatusChange={handleStatusChange} />
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleCancel} color="primary">
+        <Button onClick={handleSubmit(false)} color="primary">
           Cancel
         </Button>
-        <Button onClick={handleSave} color="primary">
+        <Button onClick={handleSubmit(true)} color="primary">
           Save
         </Button>
       </DialogActions>

ab5d6e5b590b8b6f149b141c57daaa5b729fdcfb
talita <polikashina@gmail.com>
2020-10-15 12:27:48 +0200

    add user status icons

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import UserStatus from '../types/UserStatus';
-
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+// icons
+import NewUserIcon from '@material-ui/icons/FiberNewOutlined';
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUserOutlined';
+import VIPUserIcon from '@material-ui/icons/EmojiEventsOutlined';
 
 type UserProps = {
   name: string,
   status: UserStatus,
 }
 
-function User({ name, status}: UserProps) {
+const statusIcons = {
+  [UserStatus.new]: <NewUserIcon />,
+  [UserStatus.verified]: <VerifiedUserIcon />,
+  [UserStatus.VIP]: <VIPUserIcon />,
+}
+
+function User({ name, status }: UserProps) {
   return (
-    <div>
-      <div>{name}: {status}</div>
-    </div>
+    <ListItem button>
+      <ListItemIcon>
+        {statusIcons[status]}
+      </ListItemIcon>
+      <ListItemText primary={name} />
+    </ListItem>
   );
 }
 

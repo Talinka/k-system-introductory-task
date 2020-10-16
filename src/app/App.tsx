d0df3e376d81f37c0d3d404c06f9aafd86f66fc6
talita <polikashina@gmail.com>
2020-10-16 12:49:00 +0200

    add user list fetching

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Main from '../components/Main';
 
-import User from '../types/User';
-import UserStatus from '../types/UserStatus';
-
-const users: Array<User> = [
-  { id: 1, name: "Ivan", status: UserStatus.VIP },
-  { id: 2, name: "Pert", status: UserStatus.New },
-  { id: 3, name: "Michal", status: UserStatus.VIP },
-  { id: 4, name: "John", status: UserStatus.Verified },
-]
+import { RootState } from './rootReducer';
+import { fetchUsers } from '../slices/usersSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const {
+    users,
+    isLoading,
+    error,
+  } = useSelector((state: RootState) => state.users)
+
+  useEffect(() => {
+    dispatch(fetchUsers);
+  },
+  // when we have server api for change user state, we'll need dispatch in dependency list
+  // eslint-disable-next-line
+  []);
+
+  if (error) {
+    return (
+      <div>
+        <h1>Something went wrong...</h1>
+        <div>{error}</div>
+      </div>
+    )
+  }
+
+  const renderLoading = () => (
+    <div>Loading...</div>
+  )
 
   return (
     <div>
       <h2>User list</h2>
-      {users.length > 0 && <Main users={users}/>}
+      {isLoading ? renderLoading() : <Main users={users} />}
     </div>
   );
 }

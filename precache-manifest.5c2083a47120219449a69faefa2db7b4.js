1fe1f00765038cbbcad6522258eff52315cf6848
talita <polikashina@gmail.com>
2020-10-16 13:03:54 +0200

    Updates

---
+++ precache-manifest.5c2083a47120219449a69faefa2db7b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333a2b502ee8e8926f38c487ac269dc1",
+    "url": "/k-system-introductory-task/index.html"
+  },
+  {
+    "revision": "fb8fcc3ca42db9502efe",
+    "url": "/k-system-introductory-task/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7f9886880f9d6ba269c0",
+    "url": "/k-system-introductory-task/static/js/2.6f8f7809.chunk.js"
+  },
+  {
+    "revision": "79c5c5c13f78a9e31fa837276e38ab0b",
+    "url": "/k-system-introductory-task/static/js/2.6f8f7809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb8fcc3ca42db9502efe",
+    "url": "/k-system-introductory-task/static/js/main.27f0946d.chunk.js"
+  },
+  {
+    "revision": "d033d6b7b0283f58a233",
+    "url": "/k-system-introductory-task/static/js/runtime-main.725c5cc5.js"
+  }
+]);
